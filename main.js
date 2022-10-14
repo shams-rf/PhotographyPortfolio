fb7483dad98ffed195dad57e5aca420f28720bcf
Shamsuddin Refaei <shamss.aldin@gmail.com>
2022-10-14 17:40:21 +0100

    -Make website responsive for mobile devices -Add custom scrollbar -Fix bug where nav links disappear in desktop mode

--- main.js
+++ main.js
@@ -53,15 +53,8 @@ const closeNav = () => {
 
 closeNavBtn.addEventListener('click', closeNav);
 
-nav.querySelectorAll('li a').forEach(navLink => {
-	navLink.addEventListener('click', closeNav);
-})
-
-
-
-
-
-
-
-
-
+if(document.body.clientWidth < 1024) {
+	nav.querySelectorAll('li a').forEach(navLink => {
+		navLink.addEventListener('click', closeNav);
+	})
+}
